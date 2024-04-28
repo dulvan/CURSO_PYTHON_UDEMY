8dfc33f7196f86c7d5ff40c92a487eceb1529740
Dulvan Daza <dulwan33@gmail.com>
2024-04-28 15:42:20 -0400

    Se agrega carpeta 6

---
+++ 6.EntradaSalidaDatos/EntradaDatos.py
@@ -0,0 +1,7 @@
+nombre = input('Ingresa Tu Nombre:')
+edad = int(input('Ingresa tu Edad:')) # input por ser entero tipo de dato int
+precio = float(input('Ingresa el precio:')) # input por ser flotante tipo de dato float para decimales: 10.55
+
+print(nombre)
+print(edad)
+print(precio)
